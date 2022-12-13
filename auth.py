7943ee7d0c734b8f1b93290a94cfb91779ca3a9c
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-12-13 17:50:33 +0300

    user and usermanager added

--- auth.py
+++ auth.py
@@ -1,12 +1,42 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    username: str
+    password: str
+
+
+class UserManager():
+    @staticmethod
+    def create_user(user: User):
+        ...
+
+    @staticmethod
+    def delete_user(user: User):
+        ...
+
+    @staticmethod
+    def get_user(username: str):
+        ...
 
 
 class Auth(object):
 
     def login(self, username, password):
-        ...
+        user = UserManager.get_user(username)
+        if user is None:
+            return False
+        if user.password == password:
+            return True
+        return False
 
     def register(self, username, password):
-        ...
-    
+        if UserManager.get_user(username) is not None:
+            return False
+        user = User(username=username, password=password)
+        UserManager.create_user(user)
+
     def auth_check(self, username, password):
-        ...
+        if not self.login(username, password):
+            return False
+        return True
