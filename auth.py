e111fafe2408cba698961d7e56790595e092b5ac
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2023-01-03 23:50:23 +0300

    auth system

--- auth.py
+++ auth.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
 
+from db import HomeDB
+
+
+db = HomeDB()
 
 class User(BaseModel):
+    user_id: int = None
     username: str
     password: str
 
@@ -9,20 +14,27 @@ class User(BaseModel):
 class UserManager():
     @staticmethod
     def create_user(user: User):
-        ...
+        last_user = db.users.find_one(sort=[("user_id", -1)])
+        last_id = last_user["user_id"] if last_user else 0
+        user.user_id = last_id + 1
+        db.users.insert_one(user.dict())
+        return user
 
     @staticmethod
     def delete_user(user: User):
-        ...
+        db.users.delete_one({"user_id": user.user_id})
 
     @staticmethod
     def get_user(username: str):
-        ...
+        user = db.users.find_one({"username": username})
+        if user:
+            return User(**user)
+        return None
 
 
-class Auth(object):
-
-    def login(self, username, password):
+class Auth():
+    @staticmethod
+    def login(username, password):
         user = UserManager.get_user(username)
         if user is None:
             return False
@@ -30,13 +42,16 @@ class Auth(object):
             return True
         return False
 
-    def register(self, username, password):
+    @staticmethod
+    def register(username, password):
         if UserManager.get_user(username) is not None:
             return False
         user = User(username=username, password=password)
-        UserManager.create_user(user)
+        return UserManager.create_user(user)
+        
 
-    def auth_check(self, username, password):
-        if not self.login(username, password):
+    @staticmethod
+    def auth_check(username, password):
+        if not Auth.login(username, password):
             return False
         return True
