ee920988445b06d8e3890afa4acda0def0da798d
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2023-01-03 23:49:44 +0300

    refactor

--- kontrol.py
+++ kontrol.py
@@ -1,22 +1,54 @@
 
-# import RPi.GPIO as GPIO
+home = False
+try:
+    import RPi.GPIO as GPIO
+    home = True
+except:
+    home = False
 
-class Kontrol:
-    GPIO.cleanup() # Giriş çıkış pinlerinin verilerini sıfırladık
-    GPIO.setmode(GPIO.BCM) #BCM pin dizilimini seçtik
+
+if home:
     isikPin = 0
     kapiPin = 0
     pencerePin = 0
     klimaPin = 0
-    GPIO.setup(isikPin, GPIO.OUT)# Pini çıkış pini olarak tanımladık
+
+    GPIO.cleanup()  # Giriş çıkış pinlerinin verilerini sıfırladık
+    GPIO.setmode(GPIO.BCM)  # BCM pin dizilimini seçtik
+
+    GPIO.setup(isikPin, GPIO.OUT)  # Pini çıkış pini olarak tanımladık
     GPIO.setup(kapiPin, GPIO.OUT)
     GPIO.setup(pencerePin, GPIO.OUT)
     GPIO.setup(klimaPin, GPIO.OUT)
-    def isikKontrol(self, durum:bool):
-        GPIO.output(self.isikPin,durum)# Pini HIGH veya LOW yani lojik 1 veya 0 olmasını amaçladık
-    def kapiKontrol(self, durum:bool):
-        GPIO.output(self.kapiPin,durum)
-    def pencereKontrol(self, durum:bool):
-        GPIO.output(self.pencerePin,durum)
-    def klimaKontrol(self, durum:bool):
-        GPIO.output(self.klimaPin,durum)
+
+
+class Kontrol:
+
+    @staticmethod
+    def isikKontrol(durum: bool):
+        # Pini HIGH veya LOW yani lojik 1 veya 0 olmasını amaçladık
+        if home:
+            GPIO.output(isikPin, durum)
+        else:
+            print("isik kontrol", durum)
+
+    @staticmethod
+    def kapiKontrol(durum: bool):
+        if home:
+            GPIO.output(kapiPin, durum)
+        else:
+            print("kapi kontrol", durum)
+
+    @staticmethod
+    def pencereKontrol(durum: bool):
+        if home:
+            GPIO.output(pencerePin, durum)
+        else:
+            print("pencere kontrol", durum)
+
+    @staticmethod
+    def klimaKontrol(durum: bool):
+        if home:
+            GPIO.output(klimaPin, durum)
+        else:
+            print("klima kontrol", durum)
