87c735a6d31079fde011acff2778cfa69e97e058
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2023-01-03 23:51:19 +0300

    Logs pydantic model added

--- log.py
+++ log.py
@@ -1,24 +1,32 @@
 
+from pydantic import BaseModel
+
+
+class Log(BaseModel):
+    log_id: int = None
+    username: str
+    timestamp: str
+
 
 class Logger():
-
+    @staticmethod
     def login(self, username):
         ...
-
+    @staticmethod
     def enterHome(self, username):
         ...
-
+    @staticmethod
     def exitHome(self, username):
         ...
-
+    @staticmethod
     def klima(self, username, durum):
         ...
-
+    @staticmethod
     def isik(self, username, durum):
         ...
-
+    @staticmethod
     def pencere(self, username, durum):
         ...
-
+    @staticmethod
     def get_logs(self):
         ...
